457c7354f12141e3f8c0d2ed2082573a84835aa0
Pierce Darragh <pierce.darragh@gmail.com>
2016-11-23 09:30:18 -0700

    Implemented database paragraph creation
    Can now create paragraphs in the database. Pretty exciting stuff.

--- loom/database.py
+++ loom/database.py
@@ -7,6 +7,23 @@ from bson.objectid import ObjectId
 _HOST = 'localhost'
 _PORT = 27017
 
+# Connect to the database.
+_DB_CLIENT = motor.motor_tornado.MotorClient(_HOST, _PORT)
+_DB = _DB_CLIENT.inkweaver
+
+# Define our collections.
+_USERS               = _DB.users                        # type: motor.core.AgnosticCollection
+_STORIES             = _DB.stories                      # type: motor.core.AgnosticCollection
+_CHAPTERS            = _DB.chapters                     # type: motor.core.AgnosticCollection
+_PARAGRAPHS          = _DB.paragraphs                   # type: motor.core.AgnosticCollection
+_WIKIS               = _DB.wikis                        # type: motor.core.AgnosticCollection
+_CATEGORIES          = _DB.categories                   # type: motor.core.AgnosticCollection
+_PAGES               = _DB.pages                        # type: motor.core.AgnosticCollection
+_SECTIONS            = _DB.sections                     # type: motor.core.AgnosticCollection
+_LINKS               = _DB.links                        # type: motor.core.AgnosticCollection
+_STORY_REFERENCES    = _DB.story_references             # type: motor.core.AgnosticCollection
+_WIKI_REFERENCES     = _DB.wiki_references              # type: motor.core.AgnosticCollection
+
 
 def set_db_host(host):
     global _HOST
@@ -16,24 +33,6 @@ def set_db_host(host):
 def set_db_port(port):
     global _PORT
     _PORT = port
-
-
-# Connect to the database.
-_DB_CLIENT = motor.motor_tornado.MotorClient(_HOST, _PORT)
-_DB = _DB_CLIENT.inkweaver
-
-# Define our collections.
-_USERS               = _DB.users                    # type: motor.core.AgnosticCollection
-_STORIES             = _DB.stories                  # type: motor.core.AgnosticCollection
-_CHAPTERS            = _DB.chapters                 # type: motor.core.AgnosticCollection
-_PARAGRAPHS          = _DB.paragraphs               # type: motor.core.AgnosticCollection
-_WIKIS               = _DB.wikis                    # type: motor.core.AgnosticCollection
-_CATEGORIES          = _DB.categories               # type: motor.core.AgnosticCollection
-_PAGES               = _DB.pages                    # type: motor.core.AgnosticCollection
-_SECTIONS            = _DB.sections                 # type: motor.core.AgnosticCollection
-_LINKS               = _DB.links                    # type: motor.core.AgnosticCollection
-_STORY_REFERENCES    = _DB.story_references         # type: motor.core.AgnosticCollection
-_WIKI_REFERENCES     = _DB.wiki_references          # type: motor.core.AgnosticCollection
 
 
 def hex_string_to_bson_oid(s):
@@ -54,7 +53,7 @@ async def create_user(username, password, name, email, pen_name=None):
         'statistics':  None,
         'bio':         None,
     }
-    result = await _USERS.insert_one(user)          # type: pymongo.results.InsertOneResult
+    result = await _USERS.insert_one(user)              # type: pymongo.results.InsertOneResult
     return result.inserted_id
 
 
@@ -81,7 +80,7 @@ async def create_story(user_id, wiki_id, title, author_name=None, synopsis=None)
         'chapters':    list(),
         'settings':    None,
     }
-    result = await _STORIES.insert_one(story)       # type: pymongo.results.InsertOneResult
+    result = await _STORIES.insert_one(story)           # type: pymongo.results.InsertOneResult
     story_id = result.inserted_id
     await _add_story_to_user(user_id, story_id)
     return story_id
@@ -103,7 +102,7 @@ async def create_chapter(story_id, title=None):
         'paragraphs':   list(),
         'statistics':   None,
     }
-    result = await _CHAPTERS.insert_one(chapter)    # type: pymongo.results.InsertOneResult
+    result = await _CHAPTERS.insert_one(chapter)        # type: pymongo.results.InsertOneResult
     chapter_id = result.inserted_id
     await _add_chapter_to_story(story_id, chapter_id)
     return chapter_id
@@ -116,3 +115,18 @@ async def get_chapter(chapter_id):
 
 async def _add_chapter_to_story(story_id, chapter_id):
     await _STORIES.update_one({'_id': story_id}, {'$push': {'chapters': chapter_id}})
+
+
+async def create_paragraph(chapter_id, text=None):
+    paragraph = {
+        'chapter_id': chapter_id,
+        'text': text,
+        'statistics': None,
+    }
+    result = await _PARAGRAPHS.insert_one(paragraph)    # type: pymongo.results.InsertOneResult
+    paragraph_id = result.inserted_id
+    await _add_paragraph_to_chapter(chapter_id, paragraph_id)
+
+
+async def _add_paragraph_to_chapter(chapter_id, paragraph_id):
+    pass
