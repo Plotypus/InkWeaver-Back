48ce10e618fd3a7ef80be26a2c293d7577dc8d84
Charles Khong <khong.ck.charles@gmail.com>
2016-12-01 21:29:52 -0700

    Modified endpoints to include v2 of protocol
    Placed echo server at /ws/echo

--- loom/routing.py
+++ loom/routing.py
@@ -18,16 +18,17 @@ WIKI        = WIKIS     + r'/{wiki_id}'.format(wiki_id=RE_ObjectID)
 PAGE        = WIKI      + r'/{page_id}'.format(page_id=RE_ObjectID)
 
 ROUTES = [
-    (r'/ws',    handlers.websockets.EchoHandler),
-    (API,       handlers.rest.RedirectHandler,      dict(url='http://plotypus.github.io/api')),  # GET: redirect to plotypus.github.io/api
+    (r'/ws/echo',   handlers.websockets.EchoHandler),
+    (r'/ws/v2',     handlers.websockets.GenericHandler),
+    (API,           handlers.rest.RedirectHandler,      dict(url='http://plotypus.github.io/api')),  # GET: redirect to plotypus.github.io/api
     #TODO: don't return a list here
-    (USERS,     handlers.rest.user.UsersHandler),  # GET: list of users; POST: create user
-    (USER,      ...),   # GET: user information
-    (STORIES,   handlers.rest.story.StoriesHandler),  # GET: list of user's stories; POST: create new story
-    (STORY,     handlers.rest.story.StoryHandler),  # GET: story content & list of chapters; POST: new chapter
-    (CHAPTER,   handlers.rest.story.ChapterHandler),  # GET: chapter content & list of paragraphs; POST: new paragraph
-    (PARAGRAPH, handlers.rest.story.ParagraphHandler),  # GET: N/A
-    (WIKIS,     ...),  # GET: list of user's wikis; POST: create new wiki
-    (WIKI,      ...),  # GET: wiki information; POST: create new wiki page
-    (PAGE,      ...),  # GET: retrieve information about the page; POST: write to the page
+    (USERS,         handlers.rest.user.UsersHandler),  # GET: list of users; POST: create user
+    (USER,          ...),   # GET: user information
+    (STORIES,       handlers.rest.story.StoriesHandler),  # GET: list of user's stories; POST: create new story
+    (STORY,         handlers.rest.story.StoryHandler),  # GET: story content & list of chapters; POST: new chapter
+    (CHAPTER,       handlers.rest.story.ChapterHandler),  # GET: chapter content & list of paragraphs; POST: new paragraph
+    (PARAGRAPH,     handlers.rest.story.ParagraphHandler),  # GET: N/A
+    (WIKIS,         ...),  # GET: list of user's wikis; POST: create new wiki
+    (WIKI,          ...),  # GET: wiki information; POST: create new wiki page
+    (PAGE,          ...),  # GET: retrieve information about the page; POST: write to the page
 ]
