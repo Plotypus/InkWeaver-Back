ddd5f6030d63762000620d25470b945db2114872
Pierce Darragh <pierce.darragh@gmail.com>
2016-12-30 23:10:25 -0700

    Server start corrected
    Now properly initializes the DB client. The collection may also be specified at start-up.

--- server.py
+++ server.py
@@ -6,12 +6,14 @@ import tornado.web
 from tornado.options import define as define_option, options, parse_command_line as parse_options
 
 define_option('port', default=8080, help='run on the given port', type=int)
-define_option('db-host', default='localhost', help='address of the MongoDB server')
+define_option('db-host', default='localhost', help='address of the MongoDB server', type=str)
 define_option('db-port', default=27017, help='MongoDB connection port', type=int)
+define_option('db-collection', default='inkweaver', help='collection to use in the database', type=str)
 
 
 def start_server(db_client, port, routes):
-    app = tornado.web.Application(routes, {'db_client': db_client})
+    settings = {'db_client': db_client}
+    app = tornado.web.Application(routes, **settings)
     app.listen(port)
     print("Starting server at {}:{}".format('localhost', port))
     print("Using database at {}:{}".format(app.settings['db_client'].host, app.settings['db_client'].port))
@@ -27,6 +29,6 @@ def start_server(db_client, port, routes):
 if __name__ == '__main__':
     parse_options()
 
-    client = database.LoomMongoDBMotorTornadoClient(options.db_host, options.db_port)
+    client = database.LoomMongoDBMotorTornadoClient(options.db_collection, options.db_host, options.db_port)
 
     start_server(client, options.port, routing.ROUTES)
