ef98f2128f411b33bfa88b1228daa3cc54ba7f64
Charles Khong <khong.ck.charles@gmail.com>
2017-01-11 12:52:49 -0700

    Fixed typo in server print statement
    Host and port belong to db client, not db interface

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ def start_server(db_interface, port, routes):
     app = tornado.web.Application(routes, **settings)
     app.listen(port)
     print("Starting server at {}:{}".format('localhost', port))
-    print("Using database at {}:{}".format(app.settings['db_interface'].host, app.settings['db_interface'].port))
+    print("Using database at {}:{}".format(app.settings['db_interface'].client.host, app.settings['db_interface'].client.port))
     print("Press ^C to quit.")
     try:
         tornado.ioloop.IOLoop.current().start()
