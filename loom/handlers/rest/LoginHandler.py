3444a03273e31dde0c344115582400d15ac3637d
Charles Khong <khong.ck.charles@gmail.com>
2017-01-11 16:45:14 -0700

    Modified sessions to map to user_ids
    LoginHandler and SessionManager now map from session_id -> user_id instead of username

--- loom/handlers/rest/LoginHandler.py
+++ loom/handlers/rest/LoginHandler.py
@@ -1,5 +1,6 @@
 from .GenericHandler import GenericHandler
 
+from loom.database.interfaces import AbstractDBInterface
 
 class LoginHandler(GenericHandler):
     async def post(self):
@@ -9,13 +10,19 @@ class LoginHandler(GenericHandler):
         if username is not None and password is not None:
             db_interface = self.settings['db_interface']
             if await db_interface.password_is_valid_for_username(username, password):
+                user_id = await self._get_user_id_for_username(username)
                 session_manager = self.settings['session_manager']
-                session_id = session_manager.generate_session_id(username)
+                session_id = session_manager.generate_session_id(user_id)
                 self.set_secure_session_cookie(session_id)
             else:
                 self.send_error(401)
         else:
             self.send_error(422)
+
+    async def _get_user_id_for_username(self, username):
+        db_interface: AbstractDBInterface = self.settings['db_interface']
+        user_id = await db_interface.get_user_id_for_username(username)
+        return user_id
 
     def set_secure_session_cookie(self, session_id):
         session_cookie_name = self.settings['session_cookie_name']
