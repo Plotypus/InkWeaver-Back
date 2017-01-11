b346ab53485351d115a99de38598bb584e097d41
Charles Khong <khong.ck.charles@gmail.com>
2017-01-11 15:59:01 -0700

    Added test for getting user id

--- tests/test_db_interface.py
+++ tests/test_db_interface.py
@@ -31,3 +31,16 @@ class TestDBInterface:
         assert prefs['email'] == user['email']
         assert await self.interface.get_user_stories(inserted_id) == list()
         assert await self.interface.get_user_wikis(inserted_id) == list()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('user', [
+        {
+            'username': 'tmctest',
+            'password': 'my gr3at p4ssw0rd',
+            'name':     'Testy McTesterton',
+            'email':    'tmctest@te.st',
+        },
+    ])
+    async def test_get_user_id_for_username(self, user):
+        inserted_id = await self.interface.create_user(**user)
+        assert await self.interface.get_user_id_for_username(user['username']) == inserted_id
