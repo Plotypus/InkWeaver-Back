ac0d92f57385d448041d1a96c03d55fa9c65ff7c
Charles Khong <khong.ck.charles@gmail.com>
2017-01-11 14:27:06 -0700

    Fixed LoginHandler to set cookie correctly
    Using function to ensure cookies expire after session

--- loom/handlers/rest/LoginHandler.py
+++ loom/handlers/rest/LoginHandler.py
@@ -11,13 +11,13 @@ class LoginHandler(GenericHandler):
             if await db_interface.password_is_valid_for_username(username, password):
                 session_manager = self.settings['session_manager']
                 session_id = session_manager.generate_session_id(username)
-                self.set_secure_cookie(session_id)
+                self.set_secure_session_cookie(session_id)
             else:
                 self.send_error(401)
         else:
             self.send_error(422)
 
-    def set_session_cookie(self, session_id):
+    def set_secure_session_cookie(self, session_id):
         session_cookie_name = self.settings['session_cookie_name']
         # Have the cookie expire at the end of the session
         self.set_secure_cookie(session_cookie_name, session_id, None)
