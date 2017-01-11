0a9a2978663e374df5df35661b466e656b722775
Charles Khong <khong.ck.charles@gmail.com>
2017-01-11 13:14:26 -0700

    Fixed missing await/async in Login
    Added await and async keywords

--- loom/handlers/rest/LoginHandler.py
+++ loom/handlers/rest/LoginHandler.py
@@ -2,7 +2,7 @@ from .GenericHandler import GenericHandler
 
 
 class LoginHandler(GenericHandler):
-    def post(self):
+    async def post(self):
         data = self.decode_json(self.request.body)
         username = data.get('username')
         password = data.get('password')
@@ -10,7 +10,7 @@ class LoginHandler(GenericHandler):
             self.send_error(400)
             return
         db_interface = self.settings['db_interface']
-        if db_interface.password_is_valid_for_username(username, password):
+        if await db_interface.password_is_valid_for_username(username, password):
             # TODO: Generate session id and set the cookie
             pass
         else:
