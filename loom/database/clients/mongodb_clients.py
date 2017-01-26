7e00773cc45203177388dce7632ad43f9dc12a80
Charles Khong <khong.ck.charles@gmail.com>
2017-01-26 16:25:08 -0700

    Removed header comments in DB client
    Some were outdated and had wrong information.
    
    New header comments will be added as the data model becomes more solidified after including links, references, and aliases in the data model.

--- loom/database/clients/mongodb_clients.py
+++ loom/database/clients/mongodb_clients.py
@@ -115,29 +115,6 @@ class MongoDBClient:
                           bio: str,
                           avatar=None,
                           _id=None) -> ObjectId:
-        """Inserts a new user to the users collection.
-
-        Adds a new user to the `users` collection. Stories and wikis are
-        initialized to empty lists. Unless `_id` is provided, a random BSON
-        ObjectId will be assigned to `_id`.
-
-        Args:
-            username: The username of the user.
-            password_hash: The hash of the user's password.
-            name: The full name of the user.
-            email: The email address of the user.
-            bio: A short description of the user.
-            avatar (str): Base-64 encoded image user chooses to identify
-                themselves with.
-            _id (ObjectId): `_id` is optional, but if provided, will create a
-                user with the provided ObjectId.
-
-        Returns:
-            The ObjectId that is associated with the newly created user. If
-            `_id` was provided, `_id` will be returned. Otherwise, a randomly
-            generated BSON ObjectId will be returned.
-
-        """
         # TODO: Implement statistics.
         user = {
             'username':      username,
@@ -156,11 +133,6 @@ class MongoDBClient:
         return result.inserted_id
 
     async def get_password_hash_for_username(self, username: str) -> str:
-        """
-
-        :param user_id:
-        :return:
-        """
         user = await self.users.find_one(
             filter={'username': username},
             projection={
@@ -227,20 +199,6 @@ class MongoDBClient:
         self.assert_update_one_was_successful(update_result)
 
     async def get_user_preferences(self, user_id: ObjectId) -> Dict:
-        """Grabs the preferences for the provided user.
-
-        Finds the user in the database and extracts the fields specified by
-        The LAW Protocol.
-
-        Args:
-            user_id: BSON ObjectId of user to query for.
-
-        Returns:
-            A filtered document of the user. According to the LAW Protocol,
-            returns `username`, `name`, `email`, `bio`, and `avatar` of the
-            user.
-
-        """
         result = await self.users.find_one(
             filter={'_id': user_id},
             projection={
@@ -255,16 +213,6 @@ class MongoDBClient:
         return result
 
     async def get_user_story_ids(self, user_id: ObjectId) -> List[ObjectId]:
-        """Grabs the ObjectIds of the user's stories.
-
-        Args:
-            user_id: BSON ObjectId of user to query for.
-
-        Returns:
-            A list of the BSON ObjectIds of the `stories` that the user has
-            access to.
-
-        """
         result = await self.users.find_one(
             filter={'_id': user_id},
             projection={
@@ -275,16 +223,6 @@ class MongoDBClient:
         return result['stories']
 
     async def get_user_wiki_ids(self, user_id: ObjectId) -> List[ObjectId]:
-        """Grabs the ObjectIds of the user's wikis.
-
-        Args:
-            user_id: BSON ObjectId of user to query for.
-
-        Returns:
-            A list of the BSON ObjectIds of the `wikis` that the user has
-            access to.
-
-        """
         result = await self.users.find_one(
             filter={'_id': user_id},
             projection={
@@ -307,38 +245,6 @@ class MongoDBClient:
                            summary: str,
                            section_id: ObjectId,
                            _id=None) -> ObjectId:
-        """Inserts a new story to the stories collection.
-
-        Adds a new story to the stories collection. A section for the story
-        should be created before calling this function, in which the
-        `section_id` is specified. `_id` is optional and if provided will
-        create the story with the given `_id`, rather than the generated BSON
-        ObjectId. Currently, statistics and settings are unimplemented.
-
-        Args:
-            title: The title of the story.
-            wiki_id: The unique ID of the associated wiki.
-            user_description: A dict containing information of the story's
-                owner. A user description contains a `user_id`, `name`, and
-                `access_level`.
-
-                `user_description` args:
-                    user_id (ObjectId): The unique ID of the user.
-                    name (str): The name of the user.
-                    access_level (str): A description of the user's
-                        privileges in the story.
-            summary: A brief summary of the story.
-            section_id: The unique ID of the story's recursive section
-                representation.
-            _id (ObjectId): `_id` is optional, but if provided will create a
-                story with the provided ObjectId.
-
-        Returns:
-            The ObjectId that is associated with the newly created story. If
-            `_id` was provided, `_id` will be returned. Otherwise, a randomly
-            generated BSON ObjectId will be returned.
-
-        """
         # TODO: Implement statistics and settings.
         story = {
             'title':      title,
@@ -355,28 +261,6 @@ class MongoDBClient:
         return result.inserted_id
 
     async def create_section(self, title: str, _id=None) -> ObjectId:
-        """Inserts a new section to the sections collection.
-
-        Adds a new section to the sections collection. Sections are nodes in a
-        tree that represent a story. Each section can contain text (content) and
-        also sub-sections. As a leaf node, sections can be thought of as a
-        chapter. Pre-sections can be used to represent a prologue and
-        post-sections an epilogue. `_id` is optional, and if provided will add a
-        section to the collection with the given `_id`, otherwise a BSON
-        ObjectId will be generated in its place. Currently, statistics are not
-        implemented.
-
-        Args:
-            title: The title of the section.
-            _id (ObjectId): `_id` is optional, but if provided will create a
-                section with the provided ObjectId.
-
-        Returns:
-            The ObjectId that is associated with the newly created section. If
-            `_id` was provided, `_id`, will be returned. Otherwise, the `_id`
-            associated with the section will be returned.
-
-        """
         # TODO: Implement statistics.
         section = {
             'title':                  title,
@@ -474,30 +358,10 @@ class MongoDBClient:
         self.assert_update_one_was_successful(update_result)
 
     async def get_story(self, story_id: ObjectId) -> Dict:
-        """Grabs the information associated with the provided story.
-
-        Finds the story in the database and returns the document.
-
-        Args:
-            story_id: BSON ObjectId of story to query for.
-
-        Returns:
-            A copy of the document of the story.
-
-        """
         result = await self.stories.find_one({'_id': story_id})
         return result
 
     async def get_section(self, section_id: ObjectId) -> Dict:
-        # TODO: Add docstring
-        """
-
-        Args:
-            section_id:
-
-        Returns:
-
-        """
         result = await self.sections.find_one({'_id': section_id})
         return result
 
@@ -508,37 +372,6 @@ class MongoDBClient:
     ###########################################################################
 
     async def create_wiki(self, title: str, user_description, summary: str, segment_id: ObjectId, _id=None) -> ObjectId:
-        """Inserts a new wiki to the wikis collection.
-
-        Adds a new wiki to the wikis collection. A segment for the wiki should
-        be created before calling this function, in which the `segment_id` is
-        specified. `_id` is optional and if provided will create the wiki with
-        the given `_id`, rather than the generated BSON ObjectId. Currently,
-        statistics and settings are unimplemented.
-
-        Args:
-            title: The title of the wiki.
-            user_description: A dict containing the information of the wiki's
-                owner. A user description contains a `user_id`, `name`, and
-                `access_level`.
-
-                `user_description` args:
-                    user_id (ObjectId): The unique ID of the user.
-                    name (str): The name of the user.
-                    access_level (str): A description of the user's privileges
-                    in the story.
-            summary: A brief summary of the the wiki.
-            segment_id: The unique ID of the wiki's recursive segment
-                representation.
-            _id (ObjectId): `_id` is optional, but if provided will create a
-                wiki with the provided ObjectId.
-
-        Returns:
-            The ObjectId that is associated with the newly created wiki. If
-            `_id` was provided, `_id` will be returned. Otherwise, a randomly
-            generated BSON ObjectId will be returned.
-
-        """
         # TODO: Implement statistics and settings.
         wiki = {
             'title':      title,
@@ -554,30 +387,7 @@ class MongoDBClient:
         return result.inserted_id
 
     async def create_segment(self, title: str, _id=None) -> ObjectId:
-        """Inserts a new segment to the segments collection.
-
-        Adds a new segment to the segments collection. Segments are nodes in a
-        tree that represent a wiki. Each segment can contain sub-segments and
-        pages. Segments can be thought of as non-leaf nodes in the tree, where
-        pages are the leaf nodes. `template_headings` track headings that are
-        added to pages when pages are first created. Note, `template_headings`
-        only apply to pages directly under this segment. `_id` is optional, and
-        if provided will add a segment to the collection with the given `_id`,
-        otherwise a BSON ObjectId will be generated in its place. Currently,
-        statistics are not implemented.
-
-        Args:
-            title: The title of the wiki segment.
-            _id (ObjectId): `_id` is optional, but if provided will create a
-                segment with the provided ObjectId.
-
-        Returns:
-            The ObjectId that is associated with the newly created segment. If
-            `_id` was provided, `_id`, will be returned. Otherwise, the `_id`
-            associated with the segment will be returned.
-
-        """
-        # TODO: Implement statistics. 
+        # TODO: Implement statistics.
         segment = {
             'title':             title,
             'segments':          list(),
@@ -591,28 +401,6 @@ class MongoDBClient:
         return result.inserted_id
 
     async def create_page(self, title: str, template_headings=None, _id=None) -> ObjectId:
-        """Inserts a new page to the pages collection.
-
-        Adds a new page to the pages collection. Pages are leaf nodes in a tree
-        that represent a wiki. Each page contains `headings`, which hold the
-        content for a wiki page. `_id` is optional, and if provided will add a
-        page to the collection with the given `_id`, otherwise a BSON ObjectId
-        will be generated in its place. Currently, references and aliases are
-        not implemented.
-
-        Args:
-            title: The title of the wiki page.
-            template_headings (List[Dict]): The list of template headings to
-                inherit.
-            _id (ObjectId): `_id` is optional, but if provided will create a
-                page with the provided ObjectId.
-
-        Returns:
-            The ObjectId that is associated with the newly created page. If
-            `_id` was provided, `_id`, will be returned. Otherwise, the `_id`
-            associated with the page will be returned.
-
-        """
         # TODO: Implement references and aliases.
         page = {
             'title':      title,
@@ -738,47 +526,14 @@ class MongoDBClient:
         self.assert_update_one_was_successful(update_result)
 
     async def get_wiki(self, wiki_id: ObjectId) -> Dict:
-        """Grabs the information associated with the provided wiki.
-
-        Finds the wiki in the database and returns the document.
-
-        Args:
-            wiki_id: BSON ObjectId of wiki to query for.
-
-        Returns:
-            A copy of the document of the wiki.
-
-        """
         result = await self.wikis.find_one({'_id': wiki_id})
         return result
 
     async def get_segment(self, segment_id: ObjectId) -> Dict:
-        """Grabs the information associated with the provided segment.
-
-        Finds the segment in the database and returns the document.
-
-        Args:
-            segment_id: BSON ObjectId of segment to query for.
-
-        Returns:
-            A copy of the document of the segment.
-
-        """
         result = await self.segments.find_one({'_id': segment_id})
         return result
 
     async def get_page(self, page_id: ObjectId) -> Dict:
-        """Grabs the information associated with the provided page.
-
-        Finds the page in the database and returns the document.
-
-        Args:
-            page_id: BSON ObjectId of page to query for.
-
-        Returns:
-            A copy of the document of the page.
-
-        """
         result = await self.pages.find_one({'_id': page_id})
         return result
 
