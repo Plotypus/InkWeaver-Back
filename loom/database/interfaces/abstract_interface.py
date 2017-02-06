Plotypus/InkWeaver-Back

commit 71daa06bcfcea93fa603d70b712f8713c4fe523c

Modified add_paragraph in interface
Follows new protocol and data model.

Added function in db client to get a list of paragraph ids for a given section.

loom/database/interfaces/abstract_interface.py
@@ -87,7 +87,7 @@ class AbstractDBInterface(ABC):
         pass
 
     @abstractmethod
-    async def add_paragraph(self, section_id, text, index=None):
+    async def add_paragraph(self, section_id, text, succeeding_paragarph_id=None):
         pass
 
     @abstractmethod

loom/database/clients/mongodb_clients.py
@@ -360,6 +360,14 @@ class MongoDBClient:
         result = await self.sections.find_one({'_id': section_id})
         return result
 
+    async def get_paragraph_ids(self, section_id: ObjectId):
+        pipeline = [{'$unwind': '$content'}, {'$match': {'_id': section_id}},
+                    {'$project': {'content._id': 1, '_id': 0}}]
+        results = []
+        async for doc in self.sections.aggregate(pipeline):
+            results.append(doc['content']['_id'])
+        return results
+
     ###########################################################################
     #
     # Wiki Methods

loom/database/interfaces/mongodb_interfaces.py
@@ -152,8 +152,19 @@ class MongoDBInterface(AbstractDBInterface):
         else:
             return subsection_id
 
-    async def add_paragraph(self, section_id, text, index=None):
-        return await self.client.insert_paragraph(text, to_section_id=section_id, at_index=index)
+    async def add_paragraph(self, section_id, text, succeeding_paragraph_id=None):
+        paragraph_ids = await self.client.get_paragraph_ids(section_id)
+        try:
+            if not succeeding_paragraph_id:
+                index = None
+            else:
+                index = paragraph_ids.index(succeeding_paragraph_id)
+        except ValueError:
+            # TODO: Handle case when client provides bad paragraph_id
+            raise
+        else:
+            paragraph_id = ObjectId()
+            return await self.client.insert_paragraph(paragraph_id, text, to_section_id=section_id, at_index=index)
 
     async def get_story(self, story_id):
         story = await self.client.get_story(story_id)
@@ -179,10 +190,12 @@ class MongoDBInterface(AbstractDBInterface):
         return hierarchy
 
     async def get_section_content(self, section_id):
+        # TODO: Fix formatting of data
         section = await self.client.get_section(section_id)
         return section['content']
 
     async def set_paragraph_text(self, section_id, index, text):
+        # TODO: Fix, search by ID
         return await self.client.set_paragraph_text(text, in_section_id=section_id, at_index=index)
 
     async def delete_story(self, story_id):
@@ -282,6 +295,7 @@ class MongoDBInterface(AbstractDBInterface):
         return hierarchy
 
     async def get_page_for_hierarchy(self, page_id):
+        # TODO: Include references and aliases
         page = await self.client.get_page(page_id)
         return {
             'title':   page['title'],

commit c9009744fa212efe559ac75d32a0867bb546bf17

Fix typo in abstract interface
Typo introduced in commit 71daa06bcfcea93fa603d70b712f8713c4fe523c caused problems.

loom/database/interfaces/abstract_interface.py
@@ -100,7 +100,7 @@ class AbstractDBInterface(ABC):
         pass
 
     @abstractmethod
-    async def add_paragraph(self, section_id, text, succeeding_paragarph_id=None):
+    async def add_paragraph(self, section_id, text, succeeding_paragraph_id=None):
         pass
 
     @abstractmethod