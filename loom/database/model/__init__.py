63c7a9cddc8cc71624a37edf77486f55075ea2cb
Pierce Darragh <pierce.darragh@gmail.com>
2017-02-11 00:34:56 -0700

    Implement __init__ for model

---
+++ loom/database/model/__init__.py
@@ -0,0 +1,29 @@
+from .LOMUser import LOMUser
+
+from .LOMStory import LOMStory
+from .LOMSection import LOMSection
+
+from .LOMWiki import LOMWiki
+from .LOMSegment import LOMSegment
+from .LOMPage import LOMPage
+
+from .LOMLink import LOMLink
+from .LOMContext import LOMContext
+
+from .LOMAlias import LOMAlias
+
+__all__ = [
+    'LOMUser',
+
+    'LOMStory',
+    'LOMSection',
+
+    'LOMWiki',
+    'LOMSegment',
+    'LOMPage',
+
+    'LOMLink',
+    'LOMContext',
+
+    'LOMAlias',
+]
