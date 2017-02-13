4ac0dd976695b4b415d41df1a525021c4450c7b9
Charles Khong <khong.ck.charles@gmail.com>
2017-02-13 15:05:25 -0700

    Implement Wiki Add Messages

--- loom/dispatchers/model/wiki_messages.py
+++ loom/dispatchers/model/wiki_messages.py
@@ -26,3 +26,94 @@ class CreateWiki(Message):
 
     def dispatch(self):
         self._dispatcher.create_wiki(self.message_id, self.title, self.summary)
+
+
+###########################################################################
+#
+# Add Messages
+#
+###########################################################################
+class AddSegment(Message):
+    _required_fields = [
+        'message_id',
+        'title',
+        'parent_id',
+    ]
+
+    @auto_getattr
+    def message_id(self) -> int: pass
+
+    @auto_getattr
+    def title(self) -> str: pass
+
+    @auto_getattr
+    def parent_id(self) -> ObjectId: pass
+
+    def dispatch(self):
+        self._dispatcher.add_segment(self.message_id, self.title, self.parent_id)
+
+
+class AddTemplateHeading(Message):
+    _required_fields = [
+        'message_id',
+        'title',
+        'segment_id',
+    ]
+
+    @auto_getattr
+    def message_id(self) -> int: pass
+
+    @auto_getattr
+    def title(self) -> str: pass
+
+    @auto_getattr
+    def segment_id(self) -> ObjectId: pass
+
+    def dispatch(self):
+        self._dispatcher.add_template_heading(self.message_id, self.title, self.segment_id)
+
+
+class AddPage(Message):
+    _required_fields = [
+        'message_id',
+        'title',
+        'parent_id',
+    ]
+
+    @auto_getattr
+    def message_id(self) -> int: pass
+
+    @auto_getattr
+    def title(self) -> str: pass
+
+    @auto_getattr
+    def parent_id(self) -> ObjectId: pass
+
+    def dispatch(self):
+        self._dispatcher.add_page(self.message_id, self.title, self.parent_id)
+
+
+class AddHeading(Message):
+    _required_fields = [
+        'message_id',
+        'title',
+        'page_id',
+    ]
+    _optional_fields = [
+        'index'
+    ]
+
+    @auto_getattr
+    def message_id(self) -> int: pass
+
+    @auto_getattr
+    def title(self) -> str: pass
+
+    @auto_getattr
+    def page_id(self) -> ObjectId: pass
+
+    @auto_getattr
+    def index(self) -> int: pass
+
+    def dispatch(self):
+        self._dispatcher.add_heading(self.message_id, self.title, self.page_id, self.index)
