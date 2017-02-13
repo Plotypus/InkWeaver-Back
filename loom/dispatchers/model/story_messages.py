62bc3c8139da06fc98119a80b636dde5e2236db9
Charles Khong <khong.ck.charles@gmail.com>
2017-02-12 21:08:01 -0700

    Move initialization to base Message class
    Base Message class handles field validation and field setting.

--- loom/dispatchers/model/story_messages.py
+++ loom/dispatchers/model/story_messages.py
@@ -1,27 +1,17 @@
+from .message import Message
+
 from bson import ObjectId
 
 
-class AddSucceedingSubsection:
-    _approved_fields = {
-        'message_id': True,   # Required fields
-        'title':      True,
-        'parent_id':  True,
-        'index':      False,  # Optional fields
-    }
-
-    def __init__(self, message: dict):
-        missing_fields = [field for field, value in self._approved_fields.items()
-                          if field not in message and value is not False]
-        extra_fields = [field for field in message.keys() if field not in self._approved_fields.keys()]
-        if extra_fields:
-            raise ValueError(f"Unsupported fields: {extra_fields}")
-        if missing_fields:
-            raise ValueError(f"Missing fields: {missing_fields}")
-        # Initialize optional field
-        self._index = None
-        # Set the rest of the fields
-        for field, value in message.items():
-            setattr(self, f'_{field}', value)
+class AddSucceedingSubsection(Message):
+    _required_fields = [
+        'message_id',
+        'title',
+        'parent_id',
+    ]
+    _optional_fields = [
+        'index',
+    ]
 
     @property
     def message_id(self) -> int:
