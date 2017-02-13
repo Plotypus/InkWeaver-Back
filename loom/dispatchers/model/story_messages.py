3a6674d79c8ca80be9a4bdc51079b190d4022d26
Charles Khong <khong.ck.charles@gmail.com>
2017-02-12 21:25:49 -0700

    Implement AddPrecedingSubsection and AddInnerSubsection

--- loom/dispatchers/model/story_messages.py
+++ loom/dispatchers/model/story_messages.py
@@ -3,6 +3,11 @@ from .message import Message
 from bson import ObjectId
 
 
+###########################################################################
+#
+# Create Messages
+#
+###########################################################################
 class CreateStory(Message):
     _required_fields = [
         'message_id',
@@ -26,6 +31,65 @@ class CreateStory(Message):
     @property
     def summary(self) -> str:
         return self._summary
+
+
+###########################################################################
+#
+# Add Messages
+#
+###########################################################################
+class AddPrecedingSubsection(Message):
+    _required_fields = [
+        'message_id',
+        'title',
+        'parent_id',
+    ]
+    _optional_fields = [
+        'index',
+    ]
+
+    @property
+    def message_id(self) -> int:
+        return self._message_id
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def parent_id(self) -> ObjectId:
+        return self._parent_id
+
+    @property
+    def index(self) -> int:
+        return self._index
+
+
+class AddInnerSubsection(Message):
+    _required_fields = [
+        'message_id',
+        'title',
+        'parent_id',
+    ]
+    _optional_fields = [
+        'index',
+    ]
+
+    @property
+    def message_id(self) -> int:
+        return self._message_id
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def parent_id(self) -> ObjectId:
+        return self._parent_id
+
+    @property
+    def index(self) -> int:
+        return self._index
 
 
 class AddSucceedingSubsection(Message):
