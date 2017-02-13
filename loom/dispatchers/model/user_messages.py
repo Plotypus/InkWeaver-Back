5070a7007168b4439a8cf893e89122b6b566a0da
Charles Khong <khong.ck.charles@gmail.com>
2017-02-13 14:55:14 -0700

    Implement User Set Messages

--- loom/dispatchers/model/user_messages.py
+++ loom/dispatchers/model/user_messages.py
@@ -42,3 +42,56 @@ class GetUserWikis(Message):
 
     def dispatch(self):
         self._dispatcher.get_user_wikis(self.message_id)
+
+
+###########################################################################
+#
+# Set Messages
+#
+###########################################################################
+class SetUserName(Message):
+    _required_fields = [
+        'message_id',
+        'name',
+    ]
+
+    @auto_getattr
+    def message_id(self) -> int: pass
+
+    @auto_getattr
+    def name(self) -> str: pass
+
+    def dispatch(self):
+        self._dispatcher.set_user_name(self.message_id, self.name)
+
+
+class SetUserEmail(Message):
+    _required_fields = [
+        'message_id',
+        'email',
+    ]
+
+    @auto_getattr
+    def message_id(self) -> int: pass
+
+    @auto_getattr
+    def email(self) -> str: pass
+
+    def dispatch(self):
+        self._dispatcher.set_user_email(self.message_id, self.email)
+
+
+class SetUserBio(Message):
+    _required_fields = [
+        'message_id',
+        'bio',
+    ]
+
+    @auto_getattr
+    def message_id(self) -> int: pass
+
+    @auto_getattr
+    def bio(self) -> str: pass
+
+    def dispatch(self):
+        self._dispatcher.set_user_bio(self.message_id, self.bio)
