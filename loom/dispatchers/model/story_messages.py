8ff840789d38fdafca8ed657135a8dab6f6f9a81
Charles Khong <khong.ck.charles@gmail.com>
2017-02-12 21:18:14 -0700

    Implement CreateStory

--- loom/dispatchers/model/story_messages.py
+++ loom/dispatchers/model/story_messages.py
@@ -3,6 +3,31 @@ from .message import Message
 from bson import ObjectId
 
 
+class CreateStory(Message):
+    _required_fields = [
+        'message_id',
+        'title',
+        'wiki_id',
+        'summary',
+    ]
+
+    @property
+    def message_id(self) -> int:
+        return self._message_id
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def wiki_id(self) -> ObjectId:
+        return self._wiki_id
+
+    @property
+    def summary(self) -> str:
+        return self._summary
+
+
 class AddSucceedingSubsection(Message):
     _required_fields = [
         'message_id',
