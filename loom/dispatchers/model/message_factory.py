8f66dd136557bd3cc721c18051cdd22507d4d919
Charles Khong <khong.ck.charles@gmail.com>
2017-02-12 20:16:55 -0700

    Include `add_succeeding_subsection` to Factory

--- loom/dispatchers/model/message_factory.py
+++ loom/dispatchers/model/message_factory.py
@@ -1,3 +1,8 @@
+from .story_messages import AddSucceedingSubsection
+
+
 class MessageFactory:
-    def build_message(self, action: str, message: dict):
-        pass
+    @staticmethod
+    def build_message(action: str, message: dict):
+        if action == 'add_succeeding_subsection':
+            return AddSucceedingSubsection(message)
