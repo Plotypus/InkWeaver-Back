4d2f3ae028506da11f368d50b27352e3428c6334
Charles Khong <khong.ck.charles@gmail.com>
2017-02-13 21:43:47 -0700

    Update Message and MessageFactory
    Supports every message in protocol. 
    Message requires setting a dispatcher before use (avoids cyclic dependency).

--- loom/dispatchers/model/message.py
+++ loom/dispatchers/model/message.py
@@ -1,5 +1,3 @@
-from ..LAWProtocolDispatcher import LAWProtocolDispatcher
-
 from abc import ABC, abstractmethod
 
 
@@ -13,14 +11,13 @@ class Message(ABC):
     _required_fields = []
     _optional_fields = []
 
-    def __init__(self, dispatcher: LAWProtocolDispatcher, message: dict):
-        self._dispatcher = dispatcher
+    def __init__(self, message: dict):
         missing_fields = [field for field in self._required_fields if field not in message]
         extra_fields = [field for field in message if field not in self.all_fields]
         if extra_fields:
-            raise ValueError(f"Unsupported fields: {extra_fields}")
+            raise TypeError(f"Unsupported fields: {extra_fields}")
         if missing_fields:
-            raise ValueError(f"Missing fields: {missing_fields}")
+            raise TypeError(f"Missing fields: {missing_fields}")
         # Initialize optional fields
         for field in self._optional_fields:
             setattr(self, f'_{field}', None)
@@ -31,6 +28,9 @@ class Message(ABC):
     @property
     def all_fields(self):
         return self._required_fields + self._optional_fields
+
+    def set_dispatcher(self, dispatcher):
+        self._dispatcher = dispatcher
 
     @abstractmethod
     def dispatch(self):
--- loom/dispatchers/model/message_factory.py
+++ loom/dispatchers/model/message_factory.py
@@ -1,10 +1,70 @@
-from ..LAWProtocolDispatcher import LAWProtocolDispatcher
-
-from .story_messages import AddSucceedingSubsection
+from .link_messages import *
+from .story_messages import *
+from .user_messages import *
+from .wiki_messages import *
 
 
 class MessageFactory:
+    _approved_methods = {
+        # User Information
+        'get_user_preferences':       GetUserPreferencesMessage,
+        'get_user_stories':           GetUserStoriesMessage,
+        'get_user_wikis':             GetUserWikisMessage,
+        'set_user_name':              SetUserNameMessage,
+        'set_user_email':             SetUserEmailMessage,
+        'set_user_bio':               SetUserBioMessage,
+        'user_login':                 UserLoginMessage,
+
+        # Stories
+        'create_story':               CreateStoryMessage,
+        'add_preceding_subsection':   AddPrecedingSubsectionMessage,
+        'add_inner_subsection':       AddInnerSubsectionMessage,
+        'add_succeeding_subsection':  AddSucceedingSubsectionMessage,
+        'add_paragraph':              AddParagraphMessage,
+        'edit_paragraph':             EditParagraphMessage,
+        'get_story_information':      GetStoryInformationMessage,
+        'get_story_hierarchy':        GetStoryHierarchyMessage,
+        'get_section_hierarchy':      GetSectionHierarchyMessage,
+        'get_section_content':        GetSectionContentMessage,
+        'delete_story':               DeleteStoryMessage,
+        'delete_section':             DeleteSectionMessage,
+        'delete_paragraph':           DeleteParagraphMessage,
+
+        # Wikis
+        'create_wiki':                CreateWikiMessage,
+        'add_segment':                AddSegmentMessage,
+        'add_template_heading':       AddTemplateHeadingMessage,
+        'add_page':                   AddPageMessage,
+        'add_heading':                AddHeadingMessage,
+        'edit_segment':               EditSegmentMessage,
+        'edit_template_heading':      EditTemplateHeadingMessage,
+        'edit_page':                  EditPageMessage,
+        'edit_heading':               EditHeadingMessage,
+        'get_wiki_information':       GetWikiInformationMessage,
+        'get_wiki_hierarchy':         GetWikiHierarchyMessage,
+        'get_wiki_segment_hierarchy': GetWikiSegmentHierarchyMessage,
+        'get_wiki_segment':           GetWikiSegmentMessage,
+        'get_wiki_page':              GetWikiPageMessage,
+        'delete_wiki':                DeleteWikiMessage,
+        'delete_segment':             DeleteSegmentMessage,
+        'delete_template_heading':    DeleteTemplateHeadingMessage,
+        'delete_page':                DeletePageMessage,
+        'delete_heading':             DeleteHeadingMessage,
+
+        # Links
+        'create_link':                CreateLinkMessage,
+        'delete_link':                DeleteLinkMessage,
+
+        # Aliases
+        'delete_alias':               DeleteAliasMessage,
+        'change_alias_name':          ChangeAliasNameMessage,
+    }
+
     @staticmethod
-    def build_message(dispatcher: LAWProtocolDispatcher, action: str, message: dict):
-        if action == 'add_succeeding_subsection':
-            return AddSucceedingSubsection(dispatcher, message)
+    def build_message(dispatcher, action: str, message: dict):
+        message_builder = MessageFactory._approved_methods.get(action)
+        if message_builder is not None:
+            message_object = message_builder(message)
+            message_object.set_dispatcher(dispatcher)
+            return message_object
+        raise ValueError
