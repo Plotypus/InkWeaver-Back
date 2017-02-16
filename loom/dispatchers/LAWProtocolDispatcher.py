437de9296c7222d64b0df908b9bf36396471fdfe
Charles Khong <khong.ck.charles@gmail.com>
2017-02-15 17:49:45 -0700

    Refactor dispatcher to use OGMs

--- loom/dispatchers/LAWProtocolDispatcher.py
+++ loom/dispatchers/LAWProtocolDispatcher.py
@@ -1,6 +1,7 @@
 import loom.serialize
 
 from .messages import IncomingMessageFactory
+from .messages.outgoing import *
 from loom.database.interfaces import AbstractDBInterface
 
 from decorator import decorator
@@ -95,14 +96,18 @@ class LAWProtocolDispatcher:
         return response
 
     async def dispatch(self, message: JSON, action: str, message_id=None):
+        # Receive the message and format it into one of our IncomingMessage objects.
         try:
             message_object = self.message_factory.build_message(self, action, message)
+        # Bad action.
         except ValueError:
             return self.format_failure_json(message_id, f"Action '{action}' not supported.")
+        # Bad message format.
         except TypeError as e:
             # TODO: Replace with with a more specific error
             message = e.args[0]
             return self.format_failure_json(message_id, message)
+        # Dispatch the IncomingMessage.
         try:
             return await message_object.dispatch()
         except LAWError as e:
@@ -124,19 +129,17 @@ class LAWProtocolDispatcher:
     @requires_login
     async def get_user_preferences(self, message_id):
         preferences = await self.db_interface.get_user_preferences(self.user_id)
-        return self.format_json(preferences, reply_to_id=message_id)
+        return GetUserPreferencesOutgoingMessage(message_id, **preferences)
 
     @requires_login
     async def get_user_stories(self, message_id):
         stories = await self.db_interface.get_user_stories(self.user_id)
-        message = {'stories': stories}
-        return self.format_json(message, reply_to_id=message_id)
+        return GetUserStoriesOutgoingMessage(message_id, stories)
 
     @requires_login
     async def get_user_wikis(self, message_id):
         wikis = await self.db_interface.get_user_wikis(self.user_id)
-        message = {'wikis': wikis}
-        return self.format_json(message, reply_to_id=message_id)
+        return GetUserWikisOutgoingMessage(message_id, wikis)
     
     @requires_login
     async def set_user_name(self, message_id, name):
@@ -159,10 +162,10 @@ class LAWProtocolDispatcher:
             return self.format_failure_json(message_id, "Already logged in.")
         if await self.db_interface.password_is_valid_for_username(username, password):
             self._user_id = await self.db_interface.get_user_id_for_username(username)
-            message = {'event': 'logged in'}
+            event = 'logged_in'
         else:
-            message = {'event': 'denied'}
-        return self.format_json(message, reply_to_id=message_id)
+            event = 'denied'
+        return UserLoginOutgoingMessage(message_id, event)
 
     ###########################################################################
     #
@@ -182,46 +185,41 @@ class LAWProtocolDispatcher:
             'users':        story['users'],
             'summary':      story['summary'],
         }
-        return self.format_json(message, reply_to_id=message_id)
+        return CreateStoryOutgoingMessage(message_id, **message)
 
     @requires_login
     async def add_preceding_subsection(self, message_id, title, parent_id, index=None):
         subsection_id = await self.db_interface.add_preceding_subsection(title, parent_id, index)
-        message = {'section_id': subsection_id}
-        return self.format_json(message, reply_to_id=message_id)
+        return AddPrecedingSubsectionOutgoingMessage(message_id, subsection_id)
 
     @requires_login
     async def add_inner_subsection(self, message_id, title, parent_id, index=None):
         subsection_id = await self.db_interface.add_inner_subsection(title, parent_id, index)
-        message = {'section_id': subsection_id}
-        return self.format_json(message, reply_to_id=message_id)
+        return AddInnerSubsectionOutgoingMessage(message_id, subsection_id)
 
     @requires_login
     async def add_succeeding_subsection(self, message_id, title, parent_id, index=None):
         subsection_id = await self.db_interface.add_succeeding_subsection(title, parent_id, index)
-        message = {'section_id': subsection_id}
-        return self.format_json(message, reply_to_id=message_id)
+        return AddSucceedingSubsectionOutgoingMessage(message_id, subsection_id)
 
     @requires_login
     async def add_paragraph(self, message_id, section_id, text, succeeding_paragraph_id=None):
-        # TODO: Decide whether or not to add more to response
         paragraph_id = await self.db_interface.add_paragraph(section_id, text, succeeding_paragraph_id)
-        return self.format_json({'paragraph_id': paragraph_id}, reply_to_id=message_id)
+        return AddParagraphOutgoingMessage(message_id, paragraph_id)
 
     @requires_login
     async def edit_paragraph(self, message_id, section_id, update, paragraph_id):
-        # TODO: Decide whether or not to add more to response
         if update['update_type'] == 'set_text':
             text = update['text']
             await self.db_interface.set_paragraph_text(section_id, paragraph_id=paragraph_id, text=text)
-            return self.format_json({}, reply_to_id=message_id)
+            return EditParagraphOutgoingMessage(message_id)
         else:
             raise LAWUnimplementedError("invalid `update_type`: {}".format(update['update_type']))
 
     @requires_login
     async def edit_section_title(self, message_id, section_id, new_title):
         await self.db_interface.set_section_title(section_id, new_title)
-        return self.format_json({}, reply_to_id=message_id)
+        return EditSectionTitleOutgoingMessage(message_id)
 
     @requires_login
     async def get_story_information(self, message_id, story_id):
@@ -233,42 +231,38 @@ class LAWProtocolDispatcher:
             'users':        story['users'],
             'summary':      story['summary'],
         }
-        return self.format_json(message, reply_to_id=message_id)
+        return GetStoryInformationOutgoingMessage(message_id, **message)
 
     @requires_login
     async def get_story_hierarchy(self, message_id, story_id):
-        message = {
-            'hierarchy': await self.db_interface.get_story_hierarchy(story_id)
-        }
-        return self.format_json(message, reply_to_id=message_id)
+        hierarchy = await self.db_interface.get_story_hierarchy(story_id)
+        return GetStoryHierarchyOutgoingMessage(message_id, hierarchy)
 
     @requires_login
     async def get_section_hierarchy(self, message_id, section_id):
-        message = {
-            'hierarchy': await self.db_interface.get_section_hierarchy(section_id)
-        }
-        return self.format_json(message, reply_to_id=message_id)
+        hierarchy = await self.db_interface.get_section_hierarchy(section_id)
+        return GetSectionHierarchyOutgoingMessage(message_id, hierarchy)
 
     @requires_login
     async def get_section_content(self, message_id, section_id):
         paragraphs = await self.db_interface.get_section_content(section_id)
         content = [{'text': paragraph['text'], 'paragraph_id': paragraph['_id']} for paragraph in paragraphs]
-        return self.format_json({'content': content}, reply_to_id=message_id)
+        return GetSectionContentOutgoingMessage(message_id, content)
 
     @requires_login
     async def delete_story(self, message_id, story_id):
         await self.db_interface.delete_story(story_id)
-        return self.format_json({'event': 'story_deleted'}, reply_to_id=message_id)
+        return DeleteStoryOutgoingMessage(message_id, "story_deleted")
 
     @requires_login
     async def delete_section(self, message_id, section_id):
         await self.db_interface.delete_section(section_id)
-        return self.format_json({'event': 'section_deleted'}, reply_to_id=message_id)
+        return DeleteSectionOutgoingMessage(message_id, "section_deleted")
 
     @requires_login
     async def delete_paragraph(self, message_id, section_id, paragraph_id):
         await self.db_interface.delete_paragraph(section_id, paragraph_id)
-        return self.format_json({'event': 'paragraph_deleted'}, reply_to_id=message_id)
+        return DeleteParagraphOutgoingMessage(message_id, "paragraph_deleted")
 
     ###########################################################################
     #
@@ -287,38 +281,34 @@ class LAWProtocolDispatcher:
             'users':        wiki['users'],
             'summary':      wiki['summary'],
         }
-        return self.format_json(message, reply_to_id=message_id)
-        pass
+        return CreateWikiOutgoingMessage(message_id, **message)
 
     @requires_login
     async def add_segment(self, message_id, title, parent_id):
         segment_id = await self.db_interface.add_child_segment(title, parent_id)
-        return self.format_json({'segment_id': segment_id}, reply_to_id=message_id)
+        return AddSegmentOutgoingMessage(message_id, segment_id)
 
     @requires_login
     async def add_template_heading(self, message_id, title, segment_id):
         await self.db_interface.add_template_heading(title, segment_id)
-        # TODO: Decide whether or not to add more to response
-        return self.format_json({}, reply_to_id=message_id)
+        return AddTemplateHeadingOutgoingMessage(message_id)
 
     @requires_login
     async def add_page(self, message_id, title, parent_id):
         page_id = await self.db_interface.create_page(title, parent_id)
-        return self.format_json({'page_id': page_id}, reply_to_id=message_id)
+        return AddPageOutgoingMessage(message_id, page_id)
 
     @requires_login
     async def add_heading(self, message_id, title, page_id, index=None):
         await self.db_interface.add_heading(title, page_id, index)
-        # TODO: Decide whether or not to add more to response
-        return self.format_json({}, reply_to_id=message_id)
+        return AddHeadingOutgoingMessage(message_id)
 
     @requires_login
     async def edit_segment(self, message_id, segment_id, update):
-        # TODO: Decide whether or not to add more to response
         if update['update_type'] == 'set_title':
             title = update['title']
             await self.db_interface.set_segment_title(title, segment_id)
-            return self.format_json({}, reply_to_id=message_id)
+            return EditSegmentOutgoingMessage(message_id)
         else:
             raise LAWUnimplementedError("invalid `update_type`: {}".format(update['update_type']))
 
@@ -328,11 +318,11 @@ class LAWProtocolDispatcher:
             title = update['title']
             await self.db_interface.set_template_heading_title(old_title=template_heading_title, new_title=title,
                                                                segment_id=segment_id)
-            return self.format_json({}, reply_to_id=message_id)
+            return EditTemplateHeadingOutgoingMessage(message_id)
         elif update['update_type'] == 'set_text':
             text = update['text']
             await self.db_interface.set_template_heading_text(template_heading_title, text, segment_id)
-            return self.format_json({}, reply_to_id=message_id)
+            return EditTemplateHeadingOutgoingMessage(message_id)
         else:
             raise LAWUnimplementedError(f"invalid `update_type`: {update['update_type']}")
 
@@ -341,21 +331,20 @@ class LAWProtocolDispatcher:
         if update['update_type'] == 'set_title':
             title = update['title']
             await self.db_interface.set_page_title(title, page_id)
-            return self.format_json({}, reply_to_id=message_id)
+            return EditPageOutgoingMessage(message_id)
         else:
             raise LAWUnimplementedError(f"invalid `update_type`: {update['update_type']}")
 
     @requires_login
     async def edit_heading(self, message_id, page_id, heading_title, update):
-        # TODO: Decide whether or not to add more to response
         if update['update_type'] == 'set_title':
             title = update['title']
             await self.db_interface.set_heading_title(old_title=heading_title, new_title=title, page_id=page_id)
-            return self.format_json({}, reply_to_id=message_id)
+            return EditHeadingOutgoingMessage(message_id)
         elif update['update_type'] == 'set_text':
             text = update['text']
             await self.db_interface.set_heading_text(heading_title, text, page_id)
-            return self.format_json({}, reply_to_id=message_id)
+            return EditHeadingOutgoingMessage(message_id)
         else:
             raise LAWUnimplementedError("invalid `update_type`: {}".format(update('update_type')))
 
@@ -368,74 +357,54 @@ class LAWProtocolDispatcher:
             'users':        wiki['users'],
             'summary':      wiki['summary'],
         }
-        return self.format_json(message, reply_to_id=message_id)
+        return GetWikiInformationOutgoingMessage(message_id, **message)
 
     @requires_login
     async def get_wiki_hierarchy(self, message_id, wiki_id):
         hierarchy = await self.db_interface.get_wiki_hierarchy(wiki_id)
         link_table = hierarchy.pop('links')
-        message = {
-            'hierarchy':  hierarchy,
-            'link_table': link_table,
-        }
-        return self.format_json(message, reply_to_id=message_id)
+        return GetWikiHierarchyOutgoingMessage(message_id, hierarchy, link_table)
 
     @requires_login
     async def get_wiki_segment_hierarchy(self, message_id, segment_id):
         hierarchy = await self.db_interface.get_segment_hierarchy(segment_id)
         link_table = hierarchy.pop('links')
-        message = {
-            'hierarchy':  hierarchy,
-            'link_table': link_table,
-        }
-        return self.format_json(message, reply_to_id=message_id)
+        return GetWikiSegmentHierarchyOutgoingMessage(message_id, hierarchy, link_table)
 
     @requires_login
     async def get_wiki_segment(self, message_id, segment_id):
         segment = await self.db_interface.get_segment(segment_id)
-        message = {
-            'title':             segment['title'],
-            'segments':          segment['segments'],
-            'pages':             segment['pages'],
-            'template_headings': segment['template_headings'],
-        }
-        return self.format_json(message, reply_to_id=message_id)
+        return GetWikiSegmentOutgoingMessage(message_id, **segment)
 
     @requires_login
     async def get_wiki_page(self, message_id, page_id):
         page = await self.db_interface.get_page(page_id)
-        message = {
-            'title':        page['title'],
-            'aliases':      page['aliases'],
-            'references':   page['references'],
-            'headings':     page['headings'],
-        }
-        return self.format_json(message, reply_to_id=message_id)
+        return GetWikiPageOutgoingMessage(message_id, **page)
 
     @requires_login
     async def delete_wiki(self, message_id, wiki_id):
         await self.db_interface.delete_wiki(self.user_id, wiki_id)
-        return self.format_json({'event': 'wiki_deleted'}, reply_to_id=message_id)
+        return DeleteWikiOutgoingMessage(message_id, "wiki_deleted")
         
     @requires_login
     async def delete_segment(self, message_id, segment_id):
         await self.db_interface.delete_segment(segment_id)
-        return self.format_json({'event': 'segment_deleted'}, reply_to_id=message_id)
+        return DeleteSegmentOutgoingMessage(message_id, "segment_deleted")
 
     @requires_login
     async def delete_template_heading(self, message_id, segment_id, template_heading_title):
         await self.db_interface.delete_template_heading(template_heading_title, segment_id)
-        return self.format_json({'event': 'template_heading_deleted'}, reply_to_id=message_id)
+        return DeleteTemplateHeadingOutgoingMessage(message_id, "template_heading_deleted")
 
     @requires_login
     async def delete_page(self, message_id, page_id):
         await self.db_interface.delete_page(page_id)
-        return self.format_json({'event': 'page_deleted'}, reply_to_id=message_id)
+        return DeletePageOutgoingMessage(message_id, "page_deleted")
 
     @requires_login
     async def delete_heading(self, message_id, heading_title, page_id):
         await self.db_interface.delete_heading(heading_title, page_id)
-        return self.format_json({'event': 'heading_deleted'}, reply_to_id=message_id)
+        return DeleteHeadingOutgoingMessage(message_id, "heading_deleted")
 
     ###########################################################################
     #
@@ -446,13 +415,12 @@ class LAWProtocolDispatcher:
     @requires_login
     async def create_link(self, message_id, story_id, section_id, paragraph_id, name, page_id):
         link_id = await self.db_interface.create_link(story_id, section_id, paragraph_id, name, page_id)
-        message = {'link_id': link_id}
-        return self.format_json(message, reply_to_id=message_id)
+        return CreateLinkOutgoingMessage(message_id, link_id)
 
     @requires_login
     async def delete_link(self, message_id, link_id):
         await self.db_interface.delete_link(link_id)
-        return self.format_json({'event': 'link_deleted'}, reply_to_id=message_id)
+        return DeleteLinkOutgoingMessage(message_id, "link_deleted")
 
     ###########################################################################
     #
@@ -464,9 +432,9 @@ class LAWProtocolDispatcher:
     @requires_login
     async def change_alias_name(self, message_id, alias_id, new_name):
         await self.db_interface.change_alias_name(alias_id, new_name)
-        return self.format_json({}, reply_to_id=message_id)
+        return ChangeAliasNameOutgoingMessage(message_id)
 
     @requires_login
     async def delete_alias(self, message_id, alias_id):
         await self.db_interface.delete_alias(alias_id)
-        return self.format_json({'event': 'alias_deleted'}, reply_to_id=message_id)
+        return DeleteAliasOutgoingMessage(message_id, "alias_deleted")
