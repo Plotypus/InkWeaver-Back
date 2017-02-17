b383b7771f3ff69a22dd5600dbc609f4651dc8be
Charles Khong <khong.ck.charles@gmail.com>
2017-02-17 16:13:10 -0700

    More dispatcher tests

--- tests/test_dispatcher.py
+++ tests/test_dispatcher.py
@@ -1,5 +1,5 @@
 from loom.database.interfaces import MongoDBAsyncioInterface
-from loom.dispatchers import LAWProtocolDispatcher
+from loom.dispatchers.LAWProtocolDispatcher import LAWProtocolDispatcher, LAWNotLoggedInError
 from loom.dispatchers.messages.outgoing import *
 
 import asyncio
@@ -27,6 +27,36 @@ class TestLAWDispatcher:
     def teardown(self):
         self.event_loop.run_until_complete(self.interface.drop_database())
         self.event_loop.close()
+
+    @pytest.mark.asyncio
+    async def test_requires_login(self):
+        self.dispatcher.set_user_id(None)
+        assert self.dispatcher.user_id is None
+        with pytest.raises(LAWNotLoggedInError):
+            await self.dispatcher.get_user_wikis(0)
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('message, action', [
+        ({'message_id': 83}, '123')
+    ])
+    async def test_bad_action(self, message, action):
+        error_json = await self.dispatcher.dispatch(message, action, message['message_id'])
+        assert error_json['reply_to_id'] == message['message_id']
+        assert not error_json['success']
+        assert f"'{action}' not supported" in error_json['reason']
+
+        error_json = await self.dispatcher.dispatch(message, action, None)
+        assert error_json.get('reply_to_id') is None
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('msg_missing, msg_extra, action', [
+        ({}, {'message_id': 1, 'extra': 24}, 'get_user_preferences')
+    ])
+    async def test_bad_message(self, msg_missing, msg_extra, action):
+        missing = await self.dispatcher.dispatch(msg_missing, action, None)
+        assert "Missing fields" in missing['reason']
+        extra = await self.dispatcher.dispatch(msg_extra, action, None)
+        assert "Unsupported fields" in extra['reason']
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize('action, msg', [
