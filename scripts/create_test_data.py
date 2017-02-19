8ea90edef9989262e199bed23a62aee5cb12f20d
Pierce Darragh <pierce.darragh@gmail.com>
2017-02-18 17:07:11 -0700

    Use authentication in data loading script
    Incorporate new authentication for MongoDB in the data loading script.

--- scripts/create_test_data.py
+++ scripts/create_test_data.py
@@ -12,7 +12,7 @@ from loom.database.interfaces import MongoDBAsyncioInterface
 import asyncio
 
 
-def main(jsonfile, db_name, blind_override=False):
+def main(jsonfile, db_name, db_user=None, db_pass=None, blind_override=False):
     if not blind_override:
         answer = input("This will drop the `{}` database... continue? [y/N] ".format(db_name))
         if not answer.lower().startswith('y'):
@@ -21,6 +21,8 @@ def main(jsonfile, db_name, blind_override=False):
         print("Continuing.")
     event_loop = asyncio.get_event_loop()
     interface = MongoDBAsyncioInterface(db_name, 'localhost', 27017)
+    if db_user and db_pass:
+        interface.authenticate_client(db_user, db_pass)
     processor = DataProcessor(interface)
     event_loop.run_until_complete(interface.client.drop_database())
     event_loop.run_until_complete(processor.load_file(jsonfile))
@@ -30,9 +32,19 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('data_file', help='Path to the data file.')
-    parser.add_argument('--db_name', default='inkweaver', help='Name of the database you want to use.')
+    parser.add_argument('--db-name', default='inkweaver', help='Name of the database you want to use.')
+    parser.add_argument('--db-user', default=None, help='Username to authenticate MongoDB.')
+    parser.add_argument('--db-pass', default=None, help='Password to authenticate MongoDB.')
     parser.add_argument('--no-ask', help='Do not ask for verification to dump existing database.',
                         action='store_true')
     args = parser.parse_args()
 
-    main(args.data_file, args.db_name, args.no_ask)
+    # Ensure either both or neither of the authentication arguments are given.
+    if args.db_user and not args.db_pass:
+        print("Cannot authenticate without password.")
+        sys.exit(1)
+    if args.db_pass and not args.db_user:
+        print("Cannot authenticate without username.")
+        sys.exit(1)
+
+    main(args.data_file, args.db_name, args.db_user, args.db_pass, args.no_ask)
