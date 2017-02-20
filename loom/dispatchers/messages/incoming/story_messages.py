d957688d75ac2c8be8f929943a35648bb8b21294
Pierce Darragh <pierce.darragh@gmail.com>
2017-02-19 17:34:48 -0700

    Add EditStoryIncomingMessage

--- loom/dispatchers/messages/incoming/story_messages.py
+++ loom/dispatchers/messages/incoming/story_messages.py
@@ -147,6 +147,26 @@ class AddParagraphIncomingMessage(IncomingMessage):
 # Edit Messages
 #
 ###########################################################################
+class EditStoryIncomingMessage(IncomingMessage):
+    _required_fields = [
+        'message_id',
+        'story_id',
+        'update',
+    ]
+
+    @auto_getattr
+    def message_id(self) -> int: pass
+
+    @auto_getattr
+    def story_id(self) -> ObjectId: pass
+
+    @auto_getattr
+    def update(self) -> dict: pass
+
+    def dispatch(self):
+        return self._dispatcher.edit_story(self.message_id, self.story_id, self.update)
+
+
 class EditParagraphIncomingMessage(IncomingMessage):
     _required_fields = [
         'message_id',
