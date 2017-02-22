f31c1856c0d8df0033c4a73202c1abe9b4e55763
Pierce Darragh <pierce.darragh@gmail.com>
2017-02-21 21:29:06 -0700

    Remove model
    Not currently being used, so may as well remove it for now. Helps with coverage.

--- loom/database/model/LOMBase.py
+++
@@ -1,9 +0,0 @@
-class LOMBase:
-    def __init__(self, body: dict):
-        # Takes in a dictionary assumed to describe this object.
-        self._body = body
-        # Allows for subclasses to implement special initializations.
-        self.initialize()
-
-    def initialize(self):
-        pass
