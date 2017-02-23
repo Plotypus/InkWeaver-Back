44175b6b68540c5b3f2a890d1dac2fe2fd613122
Pierce Darragh <pierce.darragh@gmail.com>
2017-02-22 23:50:32 -0700

    Update LoomServer class
    Rearranged some things to make more sense. Includes a default in the module, just like `options`.

--- loom/server.py
+++ loom/server.py
@@ -13,16 +13,20 @@ from tornado.httpserver import HTTPServer
 class LoomServer:
     def __init__(self, interface=None, session_manager=None, routes=None):
         self._interface = interface
-        self._session_manager = session_manager if session_manager is not None else SessionManager()
-        self._routes = routes if routes is not None else routing.get_routes()
+        self._session_manager = session_manager
+        self._routes = routes
 
     def create_db_interface(self, db_name, db_host, db_port, db_user=None, db_pass=None):
         self._interface = MongoDBTornadoInterface(db_name, db_host, db_port, db_user, db_pass)
 
-    def start_server(self, demo_db_host, demo_db_port, demo_db_prefix, port, ssl_crt,
-                 ssl_key, login_origin):
+    def install_demo_endpoint(self, demo_db_data_file):
+        routing.install_demo_endpoint(demo_db_data_file)
+
+    def start_server(self, demo_db_host, demo_db_port, demo_db_prefix, port, ssl_crt, ssl_key, login_origin):
         if self._interface is None:
             raise RuntimeError("cannot start server without creating a database interface")
+        session_manager = self._session_manager if self._session_manager is not None else SessionManager()
+        routes = self._routes if self._routes is not None else routing.get_routes()
         settings = {
             'db_interface':        self._interface,
             'demo_db_host':        demo_db_host,
@@ -30,10 +34,10 @@ class LoomServer:
             'demo_db_prefix':      demo_db_prefix,
             'cookie_secret':       self._generate_cookie_secret(),
             'session_cookie_name': 'loom_session',
-            'session_manager':     self._session_manager,
+            'session_manager':     session_manager,
             'login_origin':        login_origin,
         }
-        app = tornado.web.Application(self._routes, **settings)
+        app = tornado.web.Application(routes, **settings)
         if ssl_crt and ssl_key:
             ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
             ssl_ctx.load_cert_chain(ssl_crt, ssl_key)
@@ -56,3 +60,6 @@ class LoomServer:
 
     def _generate_cookie_secret(self, num_bytes=64):
         return base64.b64encode(urandom(num_bytes))
+
+
+main_server = LoomServer()
