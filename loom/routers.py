332ce52fe61e401cfcdd02d17a75073fab9728fd
Pierce Darragh <pierce.darragh@gmail.com>
2017-02-28 23:35:29 -0700

    Update Router dispatch system

--- loom/routers.py
+++ loom/routers.py
@@ -6,7 +6,7 @@ from loom.messages.incoming import (
     UnsubscribeFromStoryIncomingMessage, UnsubscribeFromWikiIncomingMessage,
 )
 from loom.messages.outgoing import (
-    BroadcastMessage, UnicastMessage, StoryBroadcastMessage, WikiBroadcastMessage, OutgoingErrorMessage,
+    UnicastMessage, StoryBroadcastMessage, WikiBroadcastMessage, OutgoingErrorMessage,
     SubscribeToStoryOutgoingMessage, SubscribeToWikiOutgoingMessage,
     UnsubscribeFromStoryOutgoingMessage, UnsubscribeFromWikiOutgoingMessage
 )
@@ -25,16 +25,26 @@ class Router:
         self.dispatcher = LAWProtocolDispatcher(interface)
         self.message_factory = IncomingMessageFactory()
         # Various dictionaries for keeping track of user information.
-        self.story_to_users: Dict[ObjectId, Set[UUID]] = defaultdict(set)
-        self.wiki_to_users: Dict[ObjectId, Set[UUID]] = defaultdict(set)
-        self.user_to_story: Dict[UUID, ObjectId] = dict()
-        self.user_to_wiki: Dict[UUID, ObjectId] = dict()
-        self.user_to_handler: Dict[UUID, LoomHandler] = dict()
+        self.story_to_uuids: Dict[ObjectId, Set[UUID]] = defaultdict(set)
+        self.wiki_to_uuids: Dict[ObjectId, Set[UUID]] = defaultdict(set)
+        self.uuid_to_user: Dict[UUID, ObjectId] = dict()
+        self.uuid_to_story: Dict[UUID, ObjectId] = dict()
+        self.uuid_to_wiki: Dict[UUID, ObjectId] = dict()
+        self.uuid_to_handler: Dict[UUID, LoomHandler] = dict()
 
     def process_incoming(self, handler: LoomHandler, message: JSON, action: str, message_id=None):
+        uuid = handler.uuid
         # Receive the message and format it into one of our IncomingMessage objects.
         try:
-            message_object: IncomingMessage = self.message_factory.build_message(self, action, message)
+            user_id = self.uuid_to_user[uuid]
+            story_id = self.uuid_to_story.get(uuid)
+            wiki_id = self.uuid_to_wiki.get(uuid)
+            additional_args = {'user_id': user_id}
+            if story_id is not None:
+                additional_args['story_id'] = story_id
+            if wiki_id is not None:
+                additional_args['wiki_id'] = wiki_id
+            message_object: IncomingMessage = self.message_factory.build_message(self, action, message, additional_args)
         # Bad action.
         except ValueError:
             return self.dispatcher.format_failure_json(message_id, f"Action '{action}' not supported.")
@@ -44,54 +54,67 @@ class Router:
             message = e.args[0]
             return self.dispatcher.format_failure_json(message_id, message)
         # Check whether the user is already connected to the router.
-        uuid = handler.uuid
-        if uuid not in self.user_to_handler:
-            self.user_to_handler[uuid] = handler
+        if uuid not in self.uuid_to_handler:
+            self.uuid_to_handler[uuid] = handler
         # Check if the new message is meant for subscription.
-        if isinstance(message, SubscriptionIncomingMessage):
-            if isinstance(message, SubscribeToStoryIncomingMessage):
-                self.subscribe_to_story(message.story_id, uuid, message_id)
-            elif isinstance(message, SubscribeToWikiIncomingMessage):
-                self.subscribe_to_wiki(message.wiki_id, uuid, message_id)
-            elif isinstance(message, UnsubscribeFromStoryIncomingMessage):
+        if isinstance(message_object, SubscriptionIncomingMessage):
+            if isinstance(message_object, SubscribeToStoryIncomingMessage):
+                self.subscribe_to_story(message_object.story_id, uuid, message_id)
+            elif isinstance(message_object, SubscribeToWikiIncomingMessage):
+                self.subscribe_to_wiki(message_object.wiki_id, uuid, message_id)
+            elif isinstance(message_object, UnsubscribeFromStoryIncomingMessage):
                 self.unsubscribe_from_story(uuid, message_id)
-            elif isinstance(message, UnsubscribeFromWikiIncomingMessage):
+            elif isinstance(message_object, UnsubscribeFromWikiIncomingMessage):
                 self.unsubscribe_from_wiki(uuid, message_id)
             else:
-                raise RuntimeError("unknown instance of SubscriptionIncomingMessage")
+                raise RuntimeError(f"unknown instance of SubscriptionIncomingMessage: {message_object}")
         else:
-            self.dispatcher.dispatch(message_object, action, message_id)
+            # Dispatch the incoming message and process the response.
+            response = self.dispatcher.dispatch(message_object, action, message_id)
+            if isinstance(response, UnicastMessage):
+                self.unicast(response)
+            elif isinstance(response, StoryBroadcastMessage):
+                self.broadcast_to_story(story_id, response)
+            elif isinstance(response, WikiBroadcastMessage):
+                self.broadcast_to_wiki(wiki_id, response)
+            else:
+                raise RuntimeError(f"unknown instance of OutgoingMessage: {response}")
+
+    def connect(self, handler: LoomHandler, user_id: ObjectId):
+        uuid = handler.uuid
+        self.uuid_to_handler[uuid] = handler
+        self.uuid_to_user[uuid] = user_id
 
     def disconnect(self, handler: LoomHandler):
         uuid = handler.uuid
         self._unsubscribe_from_story(uuid)
         self._unsubscribe_from_wiki(uuid)
-        del(self.user_to_handler[uuid])
+        del(self.uuid_to_handler[uuid])
 
     def subscribe_to_story(self, story_id: ObjectId, uuid: UUID, message_id: int):
-        if uuid in self.user_to_story:
+        if uuid in self.uuid_to_story:
             # The user is already subscribed to another story.
             err_msg = OutgoingErrorMessage(message_id, "Cannot subscribe to multiple stories.")
             self.unicast(err_msg)
         else:
-            self.story_to_users[story_id].add(uuid)
-            self.user_to_story[uuid] = story_id
+            self.story_to_uuids[story_id].add(uuid)
+            self.uuid_to_story[uuid] = story_id
             response = SubscribeToStoryOutgoingMessage(message_id, 'subscribed_to_story')
             self.unicast(response)
 
     def subscribe_to_wiki(self, wiki_id: ObjectId, uuid: UUID, message_id: int):
-        if uuid in self.user_to_wiki:
+        if uuid in self.uuid_to_wiki:
             # The user is already subscribed to another wiki.
             err_msg = OutgoingErrorMessage(message_id, "Cannot subscribe to multiple wikis.")
             self.unicast(err_msg)
         else:
-            self.wiki_to_users[wiki_id].add(uuid)
-            self.user_to_wiki[uuid] = wiki_id
+            self.wiki_to_uuids[wiki_id].add(uuid)
+            self.uuid_to_wiki[uuid] = wiki_id
             response = SubscribeToWikiOutgoingMessage(message_id, 'subscribed_to_wiki')
             self.unicast(response)
 
     def unsubscribe_from_story(self, uuid: UUID, message_id: int):
-        if uuid not in self.user_to_story:
+        if uuid not in self.uuid_to_story:
             # The user is not currently subscribed to any story.
             err_msg = OutgoingErrorMessage(message_id, "Cannot unsubscribe from story; no active story subscription.")
             self.unicast(err_msg)
@@ -101,12 +124,12 @@ class Router:
             self.unicast(response)
 
     def _unsubscribe_from_story(self, uuid: UUID):
-            story_id = self.user_to_story[uuid]
-            self.story_to_users[story_id].remove(uuid)
-            del(self.user_to_story[uuid])
+            story_id = self.uuid_to_story[uuid]
+            self.story_to_uuids[story_id].remove(uuid)
+            del(self.uuid_to_story[uuid])
 
     def unsubscribe_from_wiki(self, uuid: UUID, message_id: int):
-        if uuid not in self.user_to_wiki:
+        if uuid not in self.uuid_to_wiki:
             # The user is not currently subscribed to any wiki.
             err_msg = OutgoingErrorMessage(message_id, "Cannot unsubscribe from wiki; no active wiki subscription.")
             self.unicast(err_msg)
@@ -116,16 +139,21 @@ class Router:
             self.unicast(response)
 
     def _unsubscribe_from_wiki(self, uuid: UUID):
-            wiki_id = self.user_to_wiki[uuid]
-            self.wiki_to_users[wiki_id].remove(uuid)
-            del(self.user_to_wiki[uuid])
+            wiki_id = self.uuid_to_wiki[uuid]
+            self.wiki_to_uuids[wiki_id].remove(uuid)
+            del(self.uuid_to_wiki[uuid])
 
     def unicast(self, message: UnicastMessage):
         uuid = message.identifier.uuid
-        handler = self.user_to_handler[uuid]
+        handler = self.uuid_to_handler[uuid]
         handler.write_json(message)
 
-    def broadcast(self, object_id: ObjectId, message: BroadcastMessage):
-        for uuid in self.broadcast_map[object_id]:
-            handler = self.user_to_handler[uuid]
+    def broadcast_to_story(self, story_id: ObjectId, message: StoryBroadcastMessage):
+        for uuid in self.story_to_uuids[story_id]:
+            handler = self.uuid_to_handler[uuid]
+            handler.write_json(message)
+
+    def broadcast_to_wiki(self, wiki_id: ObjectId, message: WikiBroadcastMessage):
+        for uuid in self.wiki_to_uuids[wiki_id]:
+            handler = self.uuid_to_handler[uuid]
             handler.write_json(message)
