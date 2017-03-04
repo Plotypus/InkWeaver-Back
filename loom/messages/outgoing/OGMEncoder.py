4e18baec88615c9533f1338eff9e8e311a29d4a6
Pierce Darragh <pierce.darragh@gmail.com>
2017-03-04 15:53:52 -0700

    Update JSON encoder

--- loom/messages/outgoing/OGMEncoder.py
+++ loom/messages/outgoing/OGMEncoder.py
@@ -3,12 +3,23 @@ from .outgoing_message import OutgoingMessage
 import json
 
 from bson import ObjectId
+from uuid import UUID
 
 
 class OGMEncoder(json.JSONEncoder):
     def default(self, o):
-        if isinstance(o, str):
+        if o is None:
             return o
+        if isinstance(o, (bool, str, int, float)):
+            return o
+        if isinstance(o, dict):
+            # Build dict object
+            insides = {key: self.default(value) for key, value in o.items()}
+            return insides
+        if isinstance(o, list):
+            # Recur on each of the elements in the list
+            insides = [self.default(value) for value in o]
+            return insides
         if isinstance(o, OutgoingMessage):
             # Recur on the body object
             return self.default(vars(o))
@@ -17,13 +28,7 @@ class OGMEncoder(json.JSONEncoder):
         if isinstance(o, ObjectId):
             # Tentative ObjectId format
             return {'$oid': str(o)}
-        if isinstance(o, list):
-            # Recur on each of the elements in the list
-            insides = [self.default(value) for value in o]
-            return insides
-        if isinstance(o, dict):
-            # Build dict object
-            insides = {key: self.default(value) for key, value in o.items()}
-            return insides
-        else:
+        if isinstance(o, UUID):
             return str(o)
+        else:
+            raise ValueError(f"cannot encode object: {o}")
