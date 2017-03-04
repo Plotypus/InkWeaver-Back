49356ec2f72a58bea53ba00bb52cd4c929f80b97
Pierce Darragh <pierce.darragh@gmail.com>
2017-03-04 15:38:40 -0700

    Revise data loading

--- loom/data_processor.py
+++ loom/data_processor.py
@@ -1,6 +1,7 @@
 from loom.dispatchers import DemoDataDispatcher
 from loom.messages import IncomingMessage, OutgoingMessage
 from loom.messages.incoming.demo import DemoIncomingMessageFactory
+from loom.messages.outgoing import GetWikiInformationOutgoingMessage, GetStoryInformationOutgoingMessage
 from loom.serialize import decode_string_to_bson, encode_bson_to_string
 
 import re
@@ -47,16 +48,28 @@ class DataProcessor:
             'wiki_id': wiki_id,
             'story_id': story_id,
         }
-        self.responses['user'] = await self.dispatcher.db_interface.get_user_preferences(user_id)
-        self.responses['wiki'] = await self.dispatcher.db_interface.get_wiki(wiki_id)
-        self.responses['story'] = await self.dispatcher.db_interface.get_story(story_id)
+        # self.responses['user'] = await self.dispatcher.db_interface.get_user_preferences(user_id)
+        wiki_dict = await self.dispatcher.db_interface.get_wiki(wiki_id)
+        wiki_message = GetWikiInformationOutgoingMessage('', '',
+                                                 wiki_title=wiki_dict['title'],
+                                                 segment_id=wiki_dict['segment_id'],
+                                                 users=wiki_dict['users'],
+                                                 summary=wiki_dict['summary'])
+        self.responses['wiki'] = wiki_message
+        story_dict = await self.dispatcher.db_interface.get_story(story_id)
+        story_message = GetStoryInformationOutgoingMessage('', '',
+                                                   story_title=story_dict['title'],
+                                                   section_id=story_dict['section_id'],
+                                                   wiki_id=story_dict['wiki_id'],
+                                                   users=story_dict['users'])
+        self.responses['story'] = story_message
         for dispatch_item in dispatch_list:
             revised: JSON = {k: self.replace_id(v) for k, v in dispatch_item.items()}
             action = revised.pop('action')
             message: IncomingMessage = self.message_factory.build_message(self.dispatcher, action, revised, additional_args)
             response: OutgoingMessage = await message.dispatch()
             if message.message_id is not None:
-                self.responses[message.message_id] = response
+                self.responses[str(message.message_id)] = response
 
     id_regex = re.compile(r'\$\{([^}]+)\}')
 
@@ -72,7 +85,6 @@ class DataProcessor:
         fullmatch = re.fullmatch(self.id_regex, value)
         if fullmatch is not None:
             m_id, keys = fullmatch.group(1).split('.', 1)
-            m_id = int(m_id)
             keys = keys.split('.')
             response = vars(self.responses[m_id])
             for key in keys:
@@ -82,7 +94,6 @@ class DataProcessor:
         for match in matches:
             string_parts.append(value[prev_end:match.start()])
             m_id, keys = match.group(1).split('.', 1)
-            m_id = int(m_id)
             keys = keys.split('.')
             response = vars(self.responses[m_id])
             for key in keys:
