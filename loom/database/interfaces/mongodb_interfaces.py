e6b6de890ed75dbd8b626589e387d5d004e4e9df
Pierce Darragh <pierce.darragh@gmail.com>
2017-03-29 16:05:57 -0600

    Separate interface errors into their own file

--- loom/database/interfaces/mongodb_interfaces.py
+++ loom/database/interfaces/mongodb_interfaces.py
@@ -1,4 +1,5 @@
 from .abstract_interface import AbstractDBInterface
+from .errors import *
 
 from loom.database.clients import *
 from loom.serialize import decode_string_to_bson, encode_bson_to_string
@@ -27,23 +28,6 @@ def generate_link_format_regex():
     # Note: re.escape also escapes spaces, which is why the replace looks for '\\ '.
     pattern = re.escape(bson_string).replace(str(o), inner_regex).replace('\\ ', r'\s*')
     return re.compile(pattern)
-
-
-class InterfaceError(Exception):
-    def __init__(self, message: str, *, query: str):
-        self.message = message
-        self.query = query
-
-
-class BadValueError(InterfaceError):
-    def __init__(self, *, query: str, value):
-        super().__init__('bad value supplied', query=query)
-        self.value = value
-
-
-class FailedUpdateError(InterfaceError):
-    def __init__(self, *, query: str):
-        super().__init__('unable to complete update', query=query)
 
 
 class MongoDBInterface(AbstractDBInterface):
