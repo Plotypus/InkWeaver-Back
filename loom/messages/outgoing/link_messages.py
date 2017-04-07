de1f7ad6ed74dfa05e00b86053a3f4a0bcb13beb
Charles Khong <khong.ck.charles@gmail.com>
2017-04-06 18:58:19 -0600

    Add outgoing message for created passive links

--- loom/messages/outgoing/link_messages.py
+++ loom/messages/outgoing/link_messages.py
@@ -19,6 +19,17 @@ class CreateLinkOutgoingMessage(StoryBroadcastMessage):
         self.name = name
         self.page_id = page_id
 
+
+class CreatePassiveLinkOutgoingMessage(StoryBroadcastMessage):
+    def __init__(self, uuid: UUID, message_id: int, *, passive_link_id: ObjectId, section_id: ObjectId,
+                 paragraph_id: ObjectId, name: str, page_id: ObjectId):
+        super().__init__(uuid, message_id, 'passive_link_created')
+        self.passive_link_id = passive_link_id
+        self.section_id = section_id
+        self.paragraph_id = paragraph_id
+        self.name = name
+        self.page_id = page_id
+
     
 ###########################################################################
 #
