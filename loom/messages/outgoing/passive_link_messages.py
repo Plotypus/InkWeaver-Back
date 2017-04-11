f9be1d5e87ea26fe8af0e9f9b7c2860d066408d7
Charles Khong <khong.ck.charles@gmail.com>
2017-04-10 20:37:42 -0600

    Change passive link message to match link message
    Uses alias_id and removes unnecessary fields.

--- loom/messages/outgoing/passive_link_messages.py
+++ loom/messages/outgoing/passive_link_messages.py
@@ -10,14 +10,10 @@ from uuid import UUID
 #
 ###########################################################################
 class CreatePassiveLinkOutgoingMessage(StoryBroadcastMessage):
-    def __init__(self, uuid: UUID, message_id: int, *, passive_link_id: ObjectId, section_id: ObjectId,
-                 paragraph_id: ObjectId, name: str, page_id: ObjectId):
+    def __init__(self, uuid: UUID, message_id: int, *, passive_link_id: ObjectId, alias_id: ObjectId):
         super().__init__(uuid, message_id, 'passive_link_created')
         self.passive_link_id = passive_link_id
-        self.section_id = section_id
-        self.paragraph_id = paragraph_id
-        self.name = name
-        self.page_id = page_id
+        self.alias_id = alias_id
 
 
 ###########################################################################
